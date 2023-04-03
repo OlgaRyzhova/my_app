284b2ee416e866be91d39cd0472decad643b5b5c
Olga Ryzhova <72630568+OlgaRyzhova@users.noreply.github.com>
2023-04-03 16:12:29 +0300

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Home extends Component {
+  render() {
+    return <div>
+        <h3>This is Home page to my React App</h3>
+    </div>;
+  }
+}
